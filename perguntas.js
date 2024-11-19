c5a5b11d532f18b6bded0c3afc6170d19c00c09d
eloagalliss <00001108996668sp@al.educacao.sp.gov.br>
2024-11-19 15:56:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 5 fatorial?',
+    '5 fatorial é 120'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Portugal?',
+    'A capital de Portugal é Lisboa'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2 elevado a 5?',
+    '2 elevado a 5 é 32'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz feio em Inglês?',
+    'Feio em ingles é ugly'
+)
